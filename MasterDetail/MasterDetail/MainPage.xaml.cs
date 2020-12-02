57134cf0d39a97f0d64b24ae25871068b50500e3
Opilane TTHK <opilane@tthk.ee>
2020-12-02 11:18:55 +0200

    Add project files.

---
+++ MasterDetail/MasterDetail/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            aboutList.ItemsSource = GetMenuList();
+            var homePage = typeof(Views.Dog);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(homePage));
+            IsPresented = false;
+        }
+
+        public List<MasterMenuItems> GetMenuList()  
+        {
+            var list = new List<MasterMenuItems>();
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minust",
+                Detail = "Lühike info",
+                TargetPage = typeof(Views.Dog)
+            });
+            return list;
+        }
+        private void aboutList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedItemMenu = (MasterMenuItems)e.SelectedItem;
+            Type selectedPage = selectedItemMenu.TargetPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedPage));
+        }
+    }
+}
---
+++ MasterDetail/MasterDetail/Views/Dog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.SimpleAudioPlayer;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetail.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dog : ContentPage
+    {
+        public Dog()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void bark_Pressed(object sender, EventArgs e)
+        {
+            var player = CrossSimpleAudioPlayer.Current;
+            player.Load("");
+        }
+    }
+}
